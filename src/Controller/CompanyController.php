ef88b94f7904e22e11eb301a36fafb2aea30e284
Roberto Carlos Vazquez Basulto <rvazquezbasulto@gmail.com>
2021-03-26 09:52:16 -0400

    Create view Admin

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,15 +6,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Company;
+use App\Form\CompanyType;
+use Symfony\Component\HttpFoundation\Request;
+
 class CompanyController extends AbstractController
 {
     /**
      * @Route("/company", name="company")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $companies = $entityManager
+            ->getRepository(Company::class)
+            ->findAll();
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+            'companies' => $companies,
         ]);
     }
+
+    /**
+* @param Request $request
+* @param EntityManagerInterface $entityManager
+* @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+* @Route("/company/new", name="company_new")
+*/
+public function create(Request $request, EntityManagerInterface $entityManager)
+{
+   $company = new Company();
+   $form = $this->createForm(CompanyType::class);
+   $form->setData($company);
+   $form->handleRequest($request);
+
+   if ($form->isSubmitted() && $form->isValid()) {
+       $company = $form->getData();
+
+       $entityManager->persist($company);
+       $entityManager->flush();
+
+       return $this->redirectToRoute('company');
+   }
+
+   return $this->render('company/new.html.twig', [
+       'form' => $form->createView(),
+   ]);
 }
+}
+
